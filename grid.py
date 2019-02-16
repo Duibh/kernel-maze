33d4b733188649362ce8452010f5f235638f59d1
Dave <dave@localhost.localdomain>
2019-02-16 13:56:51 +0000

    added print_maze function

--- grid.py
+++ grid.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def build_maze(m, n, swag):
     grid = []
     for i in range(m):
@@ -5,6 +7,20 @@ def build_maze(m, n, swag):
         for j in range(n):
             row.append("wall")
             grid.append(row)
+    start_i = randint(0, m-1)
+    start_j = randint(0, n-1)
+    grid[start_i][start_j] = "empty"
     return grid
 
-print(build_maze(5, 10, None))
+def print_maze(grid):
+    for row in grid:
+        printable_row = ''
+        for cell in row:
+            if cell == "wall":
+                char = '|'
+            else:
+                char = ' '
+            printable_row += char
+        print(printable_row)
+
+print_maze(build_maze(5, 10, None))
