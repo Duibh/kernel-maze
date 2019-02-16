8cebcfd5ea55ca2306e3b557c2ba5dbbce3490bf
Dave <dave@localhost.localdomain>
2019-02-16 13:38:41 +0000

    first v of build_maze function

--- grid.py
+++ grid.py
@@ -1,2 +1,10 @@
-def grid():
-    pass
+def build_maze(m, n, swag):
+    grid = []
+    for i in range(m):
+        row = []
+        for j in range(n):
+            row.append("wall")
+            grid.append(row)
+    return grid
+
+print(build_maze(5, 10, None))
